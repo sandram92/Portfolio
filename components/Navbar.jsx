00f2e5c9f1147d1f69e30a5f07fd7c87389470dc
Sandra Margol <sandramargol@Sandras-MacBook-Air.local>
2022-11-21 20:16:43 +0000

    navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -108,7 +108,7 @@ const Navbar = () => {
           className={
             navbar
               ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
-              : "fixed left-[100%]  p-10 ease-in duration-500"
+              : "fixed left-[100%]  p-10  ease-out duration-500"
           }
         >
           <div>
